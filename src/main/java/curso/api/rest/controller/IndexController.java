3765d54cfb68cee5eec923067266d02ae2173e8f
Tiago Cordeiro <tiagoezc@hotmail.com>
2022-12-03 21:03:08 -0300

    retornando usuario em json para a tela

--- src/main/java/curso/api/rest/controller/IndexController.java
+++ src/main/java/curso/api/rest/controller/IndexController.java
@@ -1,11 +1,15 @@
 package curso.api.rest.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import curso.api.rest.model.Usuario;
 
 @RestController /*Mapeio(Identifico) essa classe como controlador REST*/
 @RequestMapping(value = "/usuario") /*Mapeamento da minha requisição do controlador*/
@@ -13,22 +17,26 @@ public class IndexController {
 
 	/*Serviço Restful*/
 	@GetMapping(value = "/", produces = "application/json") /*Acessar a raiz e produzir um JSON (return json)*/
-	/*Não é obrigatorio (required = falso), então se nao for informado é nulo
-	 * Em um sistema de verdade, é bom colocar um Default value, como por exemplo, defaultValue = "Nome não foi informado"*/
-
-	public ResponseEntity init(@RequestParam(value = "nome", required = false) String nome, @RequestParam(value = "salario") Long salario) {
+	public ResponseEntity<Usuario> init() {
 		
-		System.out.println("Parametro sendo recebido, " + nome + ", salario = R$" + salario);
-		return new ResponseEntity("Olá Usuário REST Spring Boot, seu nome é " + nome + " e seu salario eh = R$" + salario, HttpStatus.OK);
-		
-		/*Passando o parametro pelo navegador, depois do "/" colocar ?=nome(nome_escolhido)
-		 * Exemplo: http://localhost:8080/usuario/?nome=tiago
-		 * 
-		 * Passando dois parametros:
-		 * http://localhost:8080/usuario/?nome=tiago&salario=200 
-		 * */
+		Usuario usuario = new Usuario();
+		usuario.setId(20L);
+		usuario.setLogin("usuario@admin.com");
+		usuario.setNome("admin");
+		usuario.setSenha("admin");
 		
 		
+		Usuario usuario2 = new Usuario();
+		usuario2.setId(8L);
+		usuario2.setLogin("Teste");
+		usuario2.setNome("usuario");
+		usuario2.setSenha("13712703saasdvcv");
+		
+		List<Usuario> usuarios = new ArrayList<Usuario>();
+		
+		usuarios.add(usuario);
+		usuarios.add(usuario2);
+		return new ResponseEntity(usuarios, HttpStatus.OK);
 	}
 	
 }
