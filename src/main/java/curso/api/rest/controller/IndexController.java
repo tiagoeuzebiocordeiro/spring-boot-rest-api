9cb01fb28ec9710815559e591df7ecfe739ea9ed
Tiago Cordeiro <tiagoezc@hotmail.com>
2022-12-03 22:40:28 -0300

    Criando servico RESTful de consulta de usuario

--- src/main/java/curso/api/rest/controller/IndexController.java
+++ src/main/java/curso/api/rest/controller/IndexController.java
@@ -1,42 +1,43 @@
 package curso.api.rest.controller;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import curso.api.rest.model.Usuario;
+import curso.api.rest.repository.UsuarioRepository;
 
 @RestController /*Mapeio(Identifico) essa classe como controlador REST*/
 @RequestMapping(value = "/usuario") /*Mapeamento da minha requisição do controlador*/
 public class IndexController {
 
+	@Autowired
+	private UsuarioRepository usuarioRepository;
+	
 	/*Serviço Restful*/
-	@GetMapping(value = "/", produces = "application/json") /*Acessar a raiz e produzir um JSON (return json)*/
-	public ResponseEntity<Usuario> init() {
+	@GetMapping(value = "/{id}", produces = "application/json") /*Acessar a raiz e produzir um JSON (return json)*/
+	public ResponseEntity<Usuario> init(@PathVariable(value = "id") Long id) {
 		
-		Usuario usuario = new Usuario();
-		usuario.setId(20L);
-		usuario.setLogin("usuario@admin.com");
-		usuario.setNome("admin");
-		usuario.setSenha("admin");
+		Optional<Usuario> usuario = usuarioRepository.findById(id);
 		
+		return new ResponseEntity<Usuario>(usuario.get(), HttpStatus.OK);
+	}
+	
+	@GetMapping(value = "/", produces = "application/json")
+	public ResponseEntity<List<Usuario>> usuarios() {
 		
-		Usuario usuario2 = new Usuario();
-		usuario2.setId(8L);
-		usuario2.setLogin("Teste");
-		usuario2.setNome("usuario");
-		usuario2.setSenha("13712703saasdvcv");
+		List<Usuario> list = (List<Usuario>) usuarioRepository.findAll();
+	
+		return new ResponseEntity<List<Usuario>>(list, HttpStatus.OK);
 		
-		List<Usuario> usuarios = new ArrayList<Usuario>();
-		
-		usuarios.add(usuario);
-		usuarios.add(usuario2);
-		return new ResponseEntity(usuarios, HttpStatus.OK);
 	}
 	
 }
