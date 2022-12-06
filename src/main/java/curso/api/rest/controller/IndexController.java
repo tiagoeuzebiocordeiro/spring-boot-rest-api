9c228c88217c586fc7ff930bd031fa3aea82df94
Tiago Cordeiro <tiagoezc@hotmail.com>
2022-12-05 21:49:31 -0300

    Configurando o contexto da aplicacao - Context Path

--- src/main/java/curso/api/rest/controller/IndexController.java
+++ src/main/java/curso/api/rest/controller/IndexController.java
@@ -6,9 +6,11 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 //import org.springframework.web.bind.annotation.RequestParam;
@@ -19,7 +21,9 @@ import curso.api.rest.repository.UsuarioRepository;
 
 
 /*POST  = SALVAR
- * GET = TRAZER INF*/
+ * GET = TRAZER INF
+ * put = att
+ * delete = delete*/
 
 
 @RestController /*Mapeio(Identifico) essa classe como controlador REST*/
@@ -58,12 +62,32 @@ public class IndexController {
 		
 	}
 	
-	@PostMapping(value = "/cadastrar", produces = "application/json")
+	@PostMapping(value = "/cadastrar/", produces = "application/json")
 	public ResponseEntity<Usuario> cadastrar(@RequestBody Usuario usuario) {
 		
 		Usuario usuarioSalvo = usuarioRepository.save(usuario); /*Save: grava no banco de dados e retorna o objeto em seu estado persistente*/
 		
 		return new ResponseEntity<Usuario>(usuarioSalvo, HttpStatus.OK);
+		
+	}
+	
+	@PutMapping(value = "/atualizar/{id}", produces = "application/json")
+	public ResponseEntity<Usuario> atualizar(@RequestBody Usuario usuario, @PathVariable Long id) {
+		
+		/*Outras rotinas antes de ATUALIZAR*/
+		
+		Usuario usuarioSalvo = usuarioRepository.save(usuario); /*Save: grava no banco de dados e retorna o objeto em seu estado persistente*/
+		
+		return new ResponseEntity<Usuario>(usuarioSalvo, HttpStatus.OK);
+		
+	}
+	
+	@DeleteMapping(value = "/deletar/{id}", produces = "application/text")
+	public String delete(@PathVariable(value = "id") Long id) {
+		
+		usuarioRepository.deleteById(id);
+		
+		return "Delete OK";
 		
 	}
 	
